2a2e198790917be1dc51def6e8ca14b20e702ea0
tldnjs9405 <tldnjs9405@naver.com>
2024-09-09 13:14:58 +0900

    [FEAT] : Trainee , Rating JAVA클래스 파일 작성

--- Java/src/space/produce/rating/Rating.java
+++ Java/src/space/produce/rating/Rating.java
@@ -1,5 +1,48 @@
 package space.produce.rating;
 
+import space.produce.trainee.Trainee;
+
 public class Rating {
+
+	private String category;
+	private Trainee trainee; // TRAINEE TABLE ->TRAINEE_ID NUMBER REFERENCES TRAINEE(ID)의미 , 나중에 객체를 불러와야 한다
+	private String grade;
+
+	Rating() {}
+
+	public Rating(String category, Trainee trainee, String grade) {
+		this.category = category;
+		this.trainee = trainee;
+		this.grade = grade;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Trainee getTrainee() {
+		return trainee;
+	}
+
+	public void setTrainee(Trainee trainee) {
+		this.trainee = trainee;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	@Override
+	public String toString() {
+		return "Rating [category=" + category + ", trainee=" + trainee + ", grade=" + grade + "]";
+	}
 
 }
--- Java/src/space/produce/trainee/Trainee.java
+++ Java/src/space/produce/trainee/Trainee.java
@@ -1,5 +1,101 @@
 package space.produce.trainee;
 
+import java.sql.Date;
+
 public class Trainee {
+
+	private int id;
+	private String name;
+	private Date birth;
+	private String sex;
+	private int height;
+	private int weight;
+	private String nationality;
+	private Date hireDate;
+
+	Trainee() {}
+
+	public Trainee(int id, String name, Date birth, String sex, int height, int weight, String nationality,
+			Date hireDate) {
+
+		this.id = id;
+		this.name = name;
+		this.birth = birth;
+		this.sex = sex;
+		this.height = height;
+		this.weight = weight;
+		this.nationality = nationality;
+		this.hireDate = hireDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Trainee [id=" + id + ", name=" + name + ", birth=" + birth + ", sex=" + sex + ", height=" + height
+				+ ", weight=" + weight + ", nationality=" + nationality + ", hireDate=" + hireDate + "]";
+	}
 
 }
