f22d8d82b3ea431118636f3b681f0cb99c4eea1a
asiae_ <asiae_@asiae>
2024-09-10 18:19:55 +0900

    [FIX] JDBCCareDao, JDBCCareHistoryDao 코드 수정

--- Java/src/space/produce/care/JDBCCareDao.java
+++ Java/src/space/produce/care/JDBCCareDao.java
@@ -1,21 +1,87 @@
 package space.produce.care;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import space.produce.util.DataSource;
 
 public class JDBCCareDao implements CareDAO {
 
 	@Override
 	public boolean insert(Care care) {
-		return false;
+		
+		boolean result = false;
+
+	    try ( Connection connection = DataSource.getDataSource();
+	        PreparedStatement pStatement = connection.prepareStatement("INSERT INTO CARE (CATEGORY, COST) VALUES (? , ? )") ) {
+
+	        pStatement.setString(1, care.getCategory());
+	        pStatement.setInt(2, care.getCost());
+
+	        int rows = pStatement.executeUpdate();
+
+	        if (rows > 0) {
+	            result = true;
+	        }
+	         
+	    } catch (SQLException e) {
+	         e.printStackTrace();
+	    }
+
+	    return result;
 	}
 
 	@Override
 	public boolean deleteById(int id) {
-		return false;
+		
+		boolean result = false;
+
+	    try ( Connection connection = DataSource.getDataSource();
+	          PreparedStatement pStatement = connection.prepareStatement("DELETE FROM CARE WHERE ID = ?") ) {
+
+	         pStatement.setInt(1, id);
+	         
+	         int rows = pStatement.executeUpdate();
+
+	         if (rows > 0) {
+	            result = true;
+	         }
+	         
+	      } catch (SQLException e) {
+	         e.printStackTrace();
+	      }
+
+	      return result;
+
 	}
 
 	@Override
 	public List<Care> selectAll() {
-		return null;
+		
+		List<Care> cares = new ArrayList<Care>(); 
+		
+		try ( Connection connection = DataSource.getDataSource();
+	          PreparedStatement pStatement = connection.prepareStatement("SELECT ID, CATEGORY, COST FROM CARE ORDER BY ID"); 
+			  ResultSet rs = pStatement.executeQuery()) 
+		{
+		
+			while( rs.next() ) {
+				Care care = new Care(); 
+				care.setId(rs.getInt("ID"));
+				care.setCategory(rs.getString("CATEGORY"));
+				care.setCost(rs.getInt("COST"));
+				
+				cares.add(care); 
+			}
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return cares; 
 	}
 }
--- Java/src/space/produce/careHistory/JDBCCareHistoryDao.java
+++ Java/src/space/produce/careHistory/JDBCCareHistoryDao.java
@@ -1,28 +1,129 @@
 package space.produce.careHistory;
 
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import space.produce.care.Care;
+import space.produce.trainee.Trainee;
+import space.produce.util.DataSource;
 
 public class JDBCCareHistoryDao implements CareHistoryDAO {
 
 	@Override
 	public boolean insert(CareHistory careHistory) {
-		return false;
+		boolean result = false;
+
+	    try ( Connection connection = DataSource.getDataSource();
+	        PreparedStatement pStatement = 
+        		connection.prepareStatement("INSERT INTO CARE_HISTORY ( IDX, CARE_DATE, CARE_ID, TRAINEE_ID ) VALUES (?, ?, ?, ?)") ) {
+	
+	    	pStatement.setInt(1, careHistory.getIdx());
+	        pStatement.setDate(2, careHistory.getCareDate());
+	        pStatement.setInt(3, careHistory.getCare().getId()); 
+	        pStatement.setInt(4, careHistory.getTrainee().getId());  
+	        
+	        int rows = pStatement.executeUpdate();
+
+	        if (rows > 0) {
+	            result = true;
+	        }
+	      
+	    } catch (SQLException e) {
+	         e.printStackTrace();
+	    }
+
+	    return result;
 	}
 
 	@Override
 	public List<CareHistory> selectAll() {
-		return null;
+		
+		List<CareHistory> careHistories = new ArrayList<CareHistory>(); 
+		
+		try ( Connection connection = DataSource.getDataSource();
+	          PreparedStatement pStatement = connection.prepareStatement("SELECT IDX, CARE_DATE, CARE_ID, TRAINEE_ID FROM CARE_HISTORY ORDER BY IDX"); 
+			  ResultSet rs = pStatement.executeQuery()) 
+		{
+		
+			while( rs.next() ) {
+				CareHistory careHistory = new CareHistory();
+				careHistory.setIdx(rs.getInt("IDX"));
+				careHistory.setCareDate(rs.getDate("CARE_DATE"));
+				careHistory.setCare(new Care( rs.getInt("CARE_ID")));
+				careHistory.setTrainee(new Trainee(rs.getInt("TRAINEE_ID")));
+				
+				careHistories.add(careHistory); 
+			}
+	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return careHistories; 
 	}
 
 	@Override
 	public List<CareHistory> selectByTraineeId(int id) {
-		return null;
+		
+		List<CareHistory> careHistories = new ArrayList<CareHistory>(); 
+		
+		String sql = ("SELECT IDX, CARE_DATE, CARE_ID, TRAINEE_ID FROM CARE_HISTORY WHERE TRAINEE_ID = ? ORDER BY IDX"); 
+		
+		try ( Connection connection = DataSource.getDataSource();
+	          PreparedStatement pStatement = connection.prepareStatement(sql)){
+			  pStatement.setInt(1, id);
+			  ResultSet rs = pStatement.executeQuery();
+			  
+			  while( rs.next() ) {
+					CareHistory careHistory = new CareHistory();
+					
+					careHistory.setIdx(rs.getInt("IDX"));
+					careHistory.setCareDate(rs.getDate("CARE_DATE"));
+					careHistory.setCare(new Care( rs.getInt("CARE_ID")));
+					careHistory.setTrainee(new Trainee(rs.getInt("TRAINEE_ID")));
+
+					careHistories.add(careHistory); 
+				}
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return careHistories; 
 	}
 
 	@Override
 	public List<CareHistory> selectByCareId(int id) {
-		return null;
+		
+		List<CareHistory> careHistories = new ArrayList<CareHistory>(); 
+		
+		String sql = ("SELECT IDX, CARE_DATE, CARE_ID, TRAINEE_ID FROM CARE_HISTORY WHERE CARE_ID = ? ORDER BY IDX"); 
+		
+		try ( Connection connection = DataSource.getDataSource();
+	          PreparedStatement pStatement = connection.prepareStatement(sql)){
+			  pStatement.setInt(1, id);
+			  ResultSet rs = pStatement.executeQuery();
+			  
+			  while( rs.next() ) {
+					CareHistory careHistory = new CareHistory();
+					
+					careHistory.setIdx(rs.getInt("IDX"));
+					careHistory.setCareDate(rs.getDate("CARE_DATE"));
+					careHistory.setCare(new Care( rs.getInt("CARE_ID")));
+					careHistory.setTrainee(new Trainee(rs.getInt("TRAINEE_ID")));
+
+					careHistories.add(careHistory); 
+				}
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return careHistories; 
 	}
-	
 }
