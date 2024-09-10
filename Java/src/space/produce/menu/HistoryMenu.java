bc90cbb5a6a6c1979e9d71ec177c96da890aa2f7
zoovely <yon041128@naver.com>
2024-09-10 22:41:33 +0900

    [FEAT] : Create Menu Package

---
+++ Java/src/space/produce/menu/HistoryMenu.java
@@ -0,0 +1,21 @@
+package space.produce.menu;
+
+public class HistoryMenu {
+
+    public void readCareHistory() {
+        // JDBCCareHistory.selectAll 호출해서 받아온 list 한 줄씩 출력
+    }
+
+    public void readLessonHistory() {
+
+    }
+
+    public void addCareHistory() {
+        // 이건 나중에
+    }
+
+    public void addLessonHistory() {
+
+    }
+
+}
