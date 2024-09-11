2a34e4643d546f02ed3696f220c8cb5633634330
gumiluv08 <gumiluv0823@gmail.com>
2024-09-11 15:57:01 +0900

    [FEAT] : DebutMenu의 addDebut, deleteDebut, updateDebut 메소드 기능 구현

--- Java/src/space/produce/menu/DebutMenu.java
+++ Java/src/space/produce/menu/DebutMenu.java
@@ -1,13 +1,16 @@
 package space.produce.menu;
 
-import java.util.ArrayList;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 import space.produce.debut.Debut;
 import space.produce.debut.DebutDao;
 import space.produce.debut.JDBCDebutDao;
 
 public class DebutMenu {
+	private Scanner scanner = new Scanner(System.in);
 
 	public void readDebut() {
 		// JDBCDebutDAO.selectAll 사용해서 받아온 list 한 줄씩 출력
@@ -18,12 +21,42 @@ public class DebutMenu {
 		// 1. i+1를 출력.(i는 0부터 시작하므로)
 		// 2. i옆에 selectAll 구문을 이용하여 받아온 리스트를 한 줄씩 출력.
 		for (int i = 0; i < debutList.size(); i++) {
-			System.out.print((i + 1) + ": " + debutList.get(i));
+			System.out.println((i + 1) + ": " + debutList.get(i));
 		}
 	}
 
 	public void addDebut() {
 		// 사용자 입력으로 name, member_count, concept, grade, debut_date 받은 후
+		System.out.println("그룹명 입력 : ");
+		String name = scanner.nextLine();
+
+		System.out.println("인원 수 입력 : ");
+		int memberCount = Integer.parseInt(scanner.nextLine());
+
+		System.out.println("콘셉트 입력 : ");
+		String concept = scanner.nextLine();
+
+		System.out.println("종합 평가 점수 입력 : ");
+		String grade = scanner.nextLine();
+
+		System.out.println("데뷔 예정일을 YYYY-MM-DD의 형식으로 입력해주세요 : ");
+		String inputDate = scanner.nextLine();
+		Date debutDate = Date.valueOf(inputDate);
+
+		//입력받은debutDate이 오늘 이후인지 체크.
+		
+
+		Debut debut = new Debut();
+		debut.setName(name);
+		debut.setMemberCount(memberCount);
+		debut.setConcept(concept);
+		debut.setGrade(grade);
+		debut.setDebutDate(debutDate);
+
+		DebutDao debutDao = new JDBCDebutDao();
+		debutDao.insert(debut);
+		System.out.println("입력이 완료되었습니다!👍");
+
 		// 그 값으로 Debut 객체 생성해서
 		// JDBCDebutDAO.insert 호출하기
 		// true 반환 받으면 성공 메시지 출력
@@ -34,13 +67,77 @@ public class DebutMenu {
 
 	public void deleteDebut() {
 		// JDBCDebutDAO.selectAll으로 받아온 list 출력 후 (넘버링해서)
-		// 사용자가 선택한 번호의 연습생 id를 JDBCTraineeDAO.delete에 넘겨 호출
-		// true 반환 받으면 성공 메시지 출력
+		DebutMenu debutMenu = new DebutMenu();
+		DebutDao debutDao = new JDBCDebutDao();
+		List<Debut> debutList = debutDao.findAll();
+		for (int i = 0; i < debutList.size(); i++) {
+			System.out.println((i + 1) + ": " + debutList.get(i));
+		}
+
+		// 사용자가 선택한 번호의 데뷔조를 JDBCTraineeDAO.delete에 넘겨 호출
+		System.out.println("✔️ 삭제할 데뷔조를 선택해주세요 : ");
+		int pickIndex = Integer.parseInt(scanner.nextLine());
+		
+		int pickId = debutList.get(pickIndex-1).getId();
+		
+		
+		System.out.println("⚠️ 해당 팀을 정말로 삭제하시겠습니까? : 1.예  2.아니오");
+		int pickDel = Integer.parseInt(scanner.nextLine());
+		if(pickDel == 1) {
+			debutDao.deleteById(pickId);
+			// true 반환 받으면 성공 메시지 출력
+			System.out.println("🪦 삭제가 완료되었습니다.");
+		}else if(pickDel ==2) {
+			deleteDebut();
+		}else {
+			System.out.println("🚨 잘못된 입력입니다.");
+			deleteDebut();
+		}
 	}
+
+	
 
 	public void updateDebut() {
 		// JDBCDebutDAO.selectAll으로 받아온 list 출력 후 (넘버링해서)
+		DebutMenu debutMenu = new DebutMenu();
+		DebutDao debutDao = new JDBCDebutDao();
+		List<Debut> debutList = debutDao.findAll();
+		for (int i = 0; i < debutList.size(); i++) {
+			System.out.println((i + 1) + ": " + debutList.get(i));
+		}
+		
 		// 사용자가 선택한 번호의 데뷔조를 수정할 것임
+		System.out.println("✔️ 수정할 데뷔조를 선택해주세요 : ");
+		int pickIndex = Integer.parseInt(scanner.nextLine());
+		
+		//고객이 입력한 인덱스의 데뷔조를 리스트에서 선택
+		Debut selectedTeam = debutList.get(pickIndex - 1);
+		
+		System.out.println("✔️ 수정할 항목을 선택해주세요 : 1. 등급 수정   2. 데뷔 예정일 수정");
+		int picNo = Integer.parseInt(scanner.nextLine());
+		
+		
+		switch(picNo) {
+		case 1 : { //1. 등급 수정
+			System.out.println("등급을 입력해주세요 : ");
+			String newGrade = scanner.nextLine();
+			selectedTeam.setGrade(newGrade);
+			debutDao.update(selectedTeam);
+			System.out.println("수정이 완료되었습니다!👍");
+			break;
+		}
+		case 2 : { //2. 데뷔 예정일 수정
+			System.out.println("데뷔 예정일을 YYYY-MM-DD의 형식으로 입력해주세요 : ");
+			String newDateInput = scanner.nextLine();
+			Date newDebutDate = Date.valueOf(newDateInput);
+			selectedTeam.setDebutDate(newDebutDate);
+			debutDao.update(selectedTeam);
+			System.out.println("수정이 완료되었습니다!👍");
+			break;
+		}
+		}
+		
+		
 		// 1. 등급 수정 2. 데뷔 예정일 수정 출력해서
 		// 사용자가 선택한 번호와 입력 받은 값으로 해당 Debut 객체 set 메소드로 수정
 		// JDBCDebutDAO.update에 수정한 객체 넘겨서 호출
