d3520bed5d7860169c67c9f470da2fd22467d4ce
ponyozoo <sj140505@gmail.com>
2024-09-12 14:40:49 +0900

    [FEAT] : Feature LessonMenu Method

--- Java/src/space/produce/menu/HistoryMenu.java
+++ Java/src/space/produce/menu/HistoryMenu.java
@@ -1,17 +1,32 @@
 package space.produce.menu;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.Scanner;
 
 import space.produce.careHistory.CareHistory;
 import space.produce.careHistory.CareHistoryDAO;
 import space.produce.careHistory.JDBCCareHistoryDao;
+import space.produce.lesson.JDBCLessonDao;
+import space.produce.lesson.Lesson;
+import space.produce.lesson.LessonDAO;
+import space.produce.lessonHistory.JDBCLessonHistoryDao;
+import space.produce.lessonHistory.LessonHistory;
+import space.produce.lessonHistory.LessonHistoryDAO;
+import space.produce.trainee.JdbcTraineeDao;
+import space.produce.trainee.Trainee;
+import space.produce.trainee.TraineeDao;
+import space.produce.util.MyScanner;
 
 public class HistoryMenu {
 	
+	public CareHistoryDAO careHistoryDao = new JDBCCareHistoryDao(); 
+	public LessonHistoryDAO lessonHistoryDao = new JDBCLessonHistoryDao();
+	private LessonDAO lessonDao = new JDBCLessonDao();
+	public TraineeDao traineeDao = new JdbcTraineeDao();
+	private MyScanner scanner = new MyScanner(new Scanner(System.in));
+	
     public void readCareHistory() {
-        CareHistoryDAO careHistoryDao = new JDBCCareHistoryDao(); 
-    	
     	List<CareHistory> careHistories = careHistoryDao.selectAll(); 
     	 
     	for ( int i = 0; i < careHistories.size(); i++ ) {
@@ -20,15 +35,62 @@ public class HistoryMenu {
     }
 
     public void readLessonHistory() {
-
+    	List<LessonHistory> lessonHistories = lessonHistoryDao.selectAll();
+    	
+    	for (int i = 0; i < lessonHistories.size(); i++) {
+    		System.out.println((i + 1) + ". " + lessonHistories.get(i));
+    	}
     }
 
     public void addCareHistory() {
-        // 이건 나중에
+    	
     }
 
     public void addLessonHistory() {
-
+    	List<Lesson> lessons = lessonDao.selectAll();
+    	List<Trainee> trainees = traineeDao.selectAll();
+    	
+    	for (int i = 0; i < lessons.size(); i++) {
+    		System.out.println((i + 1) + ". " + lessons.get(i));
+    	}
+    	
+    	int lessonNum = 0;
+    	while (true) {
+    		System.out.print("수업을 선택해주세요 : ");
+    		lessonNum = scanner.takeInt(1, lessons.size());
+    		if (lessonNum != -1)
+    			break ;
+    		System.out.println("🚨 올바른 값을 입력해주세요");
+    	}
+    	
+    	for (int i = 0; i < trainees.size(); i++) {
+    		System.out.println((i + 1) + ". " + trainees.get(i));
+    	}
+    	
+    	int traineeNum = 0;
+    	while (true) {
+    		System.out.print("연습생을 선택해주세요 : ");
+    		traineeNum = scanner.takeInt(1, trainees.size());
+    		if (traineeNum != -1)
+    			break ;
+			System.out.println("🚨 올바른 값을 입력해주세요");
+    	}    	
+    	
+    	while (true) {
+    		System.out.print("수업 날짜를 입력해주세요 (오늘이라면 엔터를 눌러주세요) : ");
+    		String lessonDateStr = scanner.takeStr();
+    		try {
+	    		Date lessonDate = lessonDateStr == "" ? null : Date.valueOf(lessonDateStr);
+				if (lessonHistoryDao.insert(new LessonHistory(0, lessonDate, lessons.get(lessonNum - 1), trainees.get(traineeNum - 1))))
+					System.out.println("✔️ 등록 완료");
+				else
+					System.out.println("❌ 등록 실패");
+    		} catch (Exception e) {    			
+    			System.out.println("🚨 올바른 값을 입력해주세요");
+    			continue ;
+    		}
+    		break ;
+    	}
     }
-
+    
 }
--- Java/src/space/produce/menu/LessonMenu.java
+++ Java/src/space/produce/menu/LessonMenu.java
@@ -1,20 +1,65 @@
 package space.produce.menu;
 
+import java.util.List;
+import java.util.Scanner;
+
+import space.produce.lesson.JDBCLessonDao;
+import space.produce.lesson.Lesson;
+import space.produce.util.MyScanner;
+
 public class LessonMenu {
+	
+	private	JDBCLessonDao dao = new JDBCLessonDao();
+	private MyScanner scanner = new MyScanner(new Scanner(System.in));
 
     public void readLesson() {
-        // JDBCLessonDAO.selectAll 사용해서 받아온 list 한 줄씩 출력
+    	List<Lesson> allLesson = dao.selectAll();
+    	
+    	for (Lesson lesson : allLesson) {
+    		System.out.println(lesson);
+    	}
     }
 
     public void addLesson() {
-        // 사용자 입력으로 카테고리, 가격 받은 후 Lesson 객체 생성하여 JDBCLessonDAO.insert 호출
-        // true 받으면 완료 메시지 출력
+    	System.out.print("수업명을 입력해주세요 : ");
+    	String category = scanner.takeStr();
+    	
+    	System.out.print("선생님 이름을 입력해주세요 : ");
+    	String trainer = scanner.takeStr();
+    	
+    	while (true) {
+    		System.out.print("수업에 필요한 총 시간을 입력해주세요 : ");
+    		int time = scanner.takeInt(0, Integer.MAX_VALUE);
+    		if (time != -1) {
+    			if (dao.insert(new Lesson(0, trainer, category, time)))
+    				System.out.println("✔️ 등록 완료");
+    			else
+    				System.out.println("❌ 등록 실패");
+    			break ;
+    		}
+    		System.out.println("🚨 올바른 값을 입력해주세요");
+    	}    	
     }
 
     public void deleteLesson() {
-        // JDBCLessonDAO.selectAll으로 받아온 list 출력 후 (넘버링 해서)
-        // 사용자가 선택한 Lesson 객체의 id로 JDBCLessonDAO.deleteById 호출
-        // true 받으면 완료 메시지 출력
+    	List<Lesson> allLesson = dao.selectAll();
+    	
+    	for (int i = 0; i < allLesson.size(); i++) {
+    		System.out.println((i + 1) + ". " + allLesson.get(i));
+    	}
+    	
+    	while (true) {
+    		System.out.print("삭제할 수업을 선택해주세요 : ");
+    		int num = scanner.takeInt(1, allLesson.size());
+    		if (num != -1) {
+    			if (dao.deleteById(allLesson.get(num - 1).getId()))
+    				System.out.println("✔️ 삭제 완료");
+				else
+					System.out.println("❌ 삭제 실패");
+    			break ;
+			}
+    		System.out.println("🚨 올바른 값을 입력해주세요");
+    	}
     }
     
 }
