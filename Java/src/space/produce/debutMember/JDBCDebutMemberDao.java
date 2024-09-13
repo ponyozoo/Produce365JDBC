c0befe8506a403f03416d258aae5015f4ac610fc
gumiluv08 <gumiluv0823@gmail.com>
2024-09-13 11:46:42 +0900

    [FIX] : selectByGroup 메소드 테이블 3개를 join하여 값을 받아오는 것으로 수정

--- Java/src/space/produce/debutMember/JDBCDebutMemberDao.java
+++ Java/src/space/produce/debutMember/JDBCDebutMemberDao.java
@@ -94,19 +94,48 @@ public class JDBCDebutMemberDao implements DebutMemberDao {
 		
 		try (Connection connection = DataSource.getDataSource();
 				PreparedStatement pStatement 
-				= connection.prepareStatement("SELECT * FROM DEBUT_MEMBER WHERE GROUP_ID = ? ORDER BY IDX");
+				= connection.prepareStatement("SELECT D.ID"
+						+ "     , D.NAME AS GROUP_NAME"
+						+ "     , D.DEBUT_DATE"
+						+ "     , M.TRAINEE_ID"
+						+ "     , T.NAME"
+						+ "     , T.BIRTH"
+						+ "     , T.SEX"
+						+ "     , T.HEIGHT"
+						+ "     , T.WEIGHT"
+						+ "     , T.NATIONALITY"
+						+ "     , T.HIRE_DATE"
+						+ "  FROM DEBUT D , DEBUT_MEMBER M , TRAINEE T"
+						+ "  WHERE D.ID = M.GROUP_ID AND M.TRAINEE_ID = T.ID"
+						+ "  AND D.ID = ?"
+						+ "  ORDER BY M.TRAINEE_ID");
 				){
 		
-			//select하는데 필요한 debutMember 의 groupId debut객체의 id를 가져다가 pStatement안의 쿼리문 속 첫번째 ?에 넣어야 함..
+			//select하는데 필요한 debutMember 의 groupId, debut의 id를 가져다가 pStatement안의 쿼리문 속 첫번째 ?에 넣어야 함..
 			pStatement.setInt(1, groupId);
 			ResultSet rs = pStatement.executeQuery();
 			
 			while(rs.next()) {
+				Debut debut = new Debut();
 				DebutMember debutMember = new DebutMember();
-				debutMember.setIdx(rs.getInt("IDX"));
-				debutMember.setGroup(new Debut(rs.getInt("GROUP_ID")));
-				debutMember.setTrainee(new Trainee(rs.getInt("TRAINEE_ID")));	
-					
+				Trainee trainee = new Trainee();
+				
+				debut.setId(rs.getInt("ID"));
+				debut.setName(rs.getString("GROUP_NAME"));
+				debut.setDebutDate(rs.getDate("DEBUT_DATE"));
+				
+				debutMember.setGroup(debut);
+				
+				trainee.setId(rs.getInt("TRAINEE_ID"));
+				trainee.setName(rs.getString("NAME"));
+				trainee.setBirth(rs.getDate("BIRTH"));
+				trainee.setSex(rs.getString("SEX"));
+				trainee.setHeight(rs.getInt("HEIGHT"));
+				trainee.setWeight(rs.getInt("WEIGHT"));
+				trainee.setNationality(rs.getString("NATIONALITY"));
+				trainee.setHireDate(rs.getDate("HIRE_DATE"));
+				
+				debutMember.setTrainee(trainee);
 				debutMembers.add(debutMember);		
 			}
 
